9a9281d2c17ff26460197089c332df5fb165dd9a
Smit Mehta <smitmehta.work@gmail.com>
2024-01-02 13:23:59 -0500

    created utils for error handling and responses

--- SLHOMES_node/backend/src/index.js
+++ SLHOMES_node/backend/src/index.js
@@ -1,18 +1,25 @@
-require('dotenv').config()
-// const mongoose = require("mongoose");
-// import { app  } from "server.js"
-// import { DB_NAME } from "./constants";
-import connectDB from "./db/db";
+import dotenv from 'dotenv';
+import { app } from './app.js';
+import connectDB from './db/db.js';
+
+dotenv.config({
+    path: "./.env"
+});
 
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000 , () => {
-        console.log("Server is Running" , process.env.PORT);
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server is Running", process.env.PORT);
+      console.log(`http://Localhost:${process.env.PORT}/`);
+    });
+    app.get('/', (req, res) => {
+        res.send("hello")
     })
     app.on("error", (error) => { 
-        console.log("ERR : ", error);
-        throw error
-    })
-}).catch((err) => {
+      console.log("ERR: ", error);
+      throw error;
+    });
+  })
+  .catch((err) => {
     console.log("MongoDB Connection Failed", err);
-})
+  });
---
+++ SLHOMES_node/backend/src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (requestHandler) =>  {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+        .catch((err) => next(err))
+    } 
+}
+
+export { asyncHandler }
+
+// const asyncHandler = (func) => async (req, res, next) => {
+//     try {
+//         await func(req, res, next)
+        
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
